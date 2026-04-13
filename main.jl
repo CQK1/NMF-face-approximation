f4781cb4ce8180c363d816fe5d8dd6bb8a174dca
Kai <qiankai.chen@uleth.ca>
2026-04-12 20:46:46 -0600

    feat: visualization logic

--- main.jl
+++ main.jl
@@ -1,13 +1,15 @@
 include("src/nmf_logic.jl")
+include("src/visualization.jl")
 
 face_image = load("data/face01.jpg") 
 shrinked_image = imresize(face_image, (100, 100))
 V = Float64.(Gray.(shrinked_image))  # transform to grayscale and convert to float matrix
 
-for r in [10, 20, 50, 100, 200]
+for r in [200]
     println("Using $r features for approximation...")
-    W, H = nmf_optimize(V, r, iterations=50)
+    W, H = NMFLogic.nmf_optimize(V, r, iterations=50)
     reconstructed_face = W * H
     error = norm(V - reconstructed_face)
     println("The error is: ", error)
+    Visualization.visualize_reconstruction(V, reconstructed_face)
 end
--- src/nmf_logic.jl
+++ src/nmf_logic.jl
@@ -1,22 +1,23 @@
-using LinearAlgebra
-using Images, FileIO
-
-function nmf_optimize(V, r; iterations=100)
-    # 获取原始数据的行数和列数
-    n, m = size(V)
-    
-    # 随机初始化 W (特征) 和 H (权重)，注意必须是非负数
-    W = rand(n, r)
-    H = rand(r, m)
-    
-    # 开始优化
-    for i in 1:iterations
-        # 更新 H 矩阵
-        H = H .* (W' * V) ./ (W' * W * H .+ 1e-9)
+module NMFLogic
+    using LinearAlgebra
+    export nmf_optimize
+    function nmf_optimize(V, r; iterations=100)
+        # 获取原始数据的行数和列数
+        n, m = size(V)
         
-        # 更新 W 矩阵
-        W = W .* (V * H') ./ (W * H * H' .+ 1e-9)
+        # 随机初始化 W (特征) 和 H (权重)，注意必须是非负数
+        W = rand(n, r)
+        H = rand(r, m)
+        
+        # 开始优化
+        for i in 1:iterations
+            # 更新 H 矩阵
+            H = H .* (W' * V) ./ (W' * W * H .+ 1e-9)
+            
+            # 更新 W 矩阵
+            W = W .* (V * H') ./ (W * H * H' .+ 1e-9)
+        end
+        
+        return W, H
     end
-    
-    return W, H
 end
